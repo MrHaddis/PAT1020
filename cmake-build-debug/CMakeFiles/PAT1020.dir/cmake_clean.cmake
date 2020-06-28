0f6b91c682727e06aea58e9703476c7feb4fbbe1
duanyuxiang <duanyx@belink.com>
2020-06-28 09:27:41 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/PAT1020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAT1020.dir/main.c.obj"
+  "PAT1020.exe"
+  "PAT1020.exe.manifest"
+  "PAT1020.pdb"
+  "libPAT1020.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PAT1020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+//MrHaddis
+//https://github.com/MrHaddis/PAT1020
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
